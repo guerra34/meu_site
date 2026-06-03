8bc2c626fd447452c661ca4abc6c848e66222537
guerra34 <guerraadriano34@gmail.com>
2026-06-03 15:54:21 -0300

    Cria a pagina inicial do siye

--- meu_site/views.py
+++ meu_site/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Olá mundo! Meu primeiro site com Django está funcionando! 🚀</h1>")
-    
+    from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Bem-vindo ao meu primeiro site em Django!</h1>")   
